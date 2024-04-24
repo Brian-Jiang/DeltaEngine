6a0d5cdf4b9dec5787f16113c725f3db725aecdf
Xiao Jiang <bjjx1999@live.com>
2024-04-25 00:22:09 +0800

    Init

---
+++ DeltaEngine.cpp
@@ -0,0 +1,14 @@
+﻿// DeltaEngine.cpp : Defines the entry point for the application.
+//
+
+#include "DeltaEngine.h"
+#include <fmt/core.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	fmt::print("Hello World!\n");
+	return 0;
+}
