7bb82f113e33e74af9cfbe7cd6cd430cc69d117b
Xiao Jiang <bjjx1999@live.com>
2026-02-12 23:00:43 -0800

    Remove gl

--- Engine/ThirdParty/glew.cmake
+++
@@ -1,9 +0,0 @@
-set(BUILD_UTILS OFF)
-set(GLEW_REGAL OFF)
-set(GLEW_OSMESA OFF)
-set(BUILD_FRAMEWORK OFF)
-
-add_subdirectory(glew/build/cmake)
-
-set_target_properties(glew PROPERTIES FOLDER ${third_party_folder})
-set_target_properties(glew_s PROPERTIES FOLDER ${third_party_folder})
