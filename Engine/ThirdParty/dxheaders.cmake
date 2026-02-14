b39d2412b8acbf58921ef6544c0e53d611b6e175
Xiao Jiang <bjjx1999@live.com>
2026-02-13 16:51:10 -0800

    Add dxheaders (unfinished)

---
+++ Engine/ThirdParty/dxheaders.cmake
@@ -0,0 +1,16 @@
+message(STATUS "====== Delta Engine ====== Configuring DirectX-Headers ======")
+
+set(dxheaders_SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/DirectX-Headers)
+
+file(GLOB dxheaders_files CONFIGURE_DEPENDS
+    "${dxheaders_SOURCE_DIR}/include/directx/d3dx12*.*"
+)
+
+add_library(dxheaders INTERFACE)
+target_sources(dxheaders INTERFACE ${dxheaders_files})
+target_include_directories(dxheaders SYSTEM INTERFACE 
+    $<BUILD_INTERFACE:${dxheaders_SOURCE_DIR}/include>
+    $<BUILD_INTERFACE:${dxheaders_SOURCE_DIR}/include/directx>
+)
+
+set_target_properties(dxheaders PROPERTIES FOLDER ${third_party_folder})
