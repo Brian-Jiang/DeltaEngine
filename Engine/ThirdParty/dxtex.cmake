7a7bf2035ce3ce3b46496cc9978b19e7aad5ee39
Xiao Jiang <bjjx1999@live.com>
2026-02-13 16:03:19 -0800

    Cleanup cmake options

--- Engine/ThirdParty/dxtex.cmake
+++ Engine/ThirdParty/dxtex.cmake
@@ -4,6 +4,7 @@ set(BUILD_SHARED_LIBS ON)
 set(BUILD_TOOLS OFF)
 set(BUILD_SAMPLE OFF)
 set(BUILD_DX11 OFF)
+set(BUILD_TESTING OFF)
 
 add_subdirectory(DirectXTex ${CMAKE_BINARY_DIR}/bin/CMake/DirectXTex)
 
--- Engine/ThirdParty/dxtk12.cmake
+++ Engine/ThirdParty/dxtk12.cmake
@@ -1,6 +1,7 @@
 message(STATUS "====== Delta Engine ====== Configuring DirectXTK12 ======")
 
 set(BUILD_SHARED_LIBS ON)
+set(BUILD_TESTING OFF)
 
 add_subdirectory(DirectXTK12 ${CMAKE_BINARY_DIR}/bin/CMake/DirectXTK12)
 
