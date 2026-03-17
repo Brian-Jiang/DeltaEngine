67e62c877d6cc3738ecae68ba7413edc5a40e34c
Xiao Jiang <bjjx1999@live.com>
2026-03-17 11:34:58 -0700

    Move assets

--- Engine/Runtime/IO/IOManager.cpp
+++ Engine/Runtime/IO/IOManager.cpp
@@ -5,50 +5,50 @@
 
 using namespace DeltaEngine;
 
-bool IOManager::readFileToBuffer(const std::string& filePath, std::vector<char>& buffer) {
-	std::ifstream file(filePath, std::ios::binary);
-	if (file.fail()) {
-		perror(filePath.c_str());
-		return false;
-	}
+//bool IOManager::readFileToBuffer(const std::string& filePath, std::vector<char>& buffer) {
+//	std::ifstream file(filePath, std::ios::binary);
+//	if (file.fail()) {
+//		perror(filePath.c_str());
+//		return false;
+//	}
+//
+//	// Seek to the end
+//	file.seekg(0, std::ios::end);
+//	 
+//	// Get the file size
+//	auto fileSize = file.tellg();
+//	file.seekg(0, std::ios::beg);
+// 
+//	// Reduce the file size by any header bytes that might be present
+//	fileSize -= file.tellg();
+// 
+//	buffer.resize(fileSize);
+//	file.read(&(buffer[0]), fileSize);
+//	file.close();
+// 
+//	return true;
+// }
 
-	// Seek to the end
-	file.seekg(0, std::ios::end);
-	 
-	// Get the file size
-	auto fileSize = file.tellg();
-	file.seekg(0, std::ios::beg);
- 
-	// Reduce the file size by any header bytes that might be present
-	fileSize -= file.tellg();
- 
-	buffer.resize(fileSize);
-	file.read(&(buffer[0]), fileSize);
-	file.close();
- 
-	return true;
- }
+//std::wstring IOManager::GetAssetFullPath(LPCWSTR assetName)
+//{
+//	std::wstring prefix = L"../../../Engine/Runtime/";
+//    return prefix + assetName;
+//}
 
-std::wstring IOManager::GetAssetFullPath(LPCWSTR assetName)
-{
-	std::wstring prefix = L"../../../Engine/Runtime/";
-    return prefix + assetName;
-}
-
-std::string IOManager::GetAssetFullPath(const std::string& assetName)
-{
-	std::string prefix = "../../../Engine/Runtime/";
-	return prefix + std::string(assetName.begin(), assetName.end());
-}
+//std::string IOManager::GetAssetFullPath(const std::string& assetName)
+//{
+//	std::string prefix = "../../../Engine/Runtime/";
+//	return prefix + std::string(assetName.begin(), assetName.end());
+//}
 
 std::wstring IOManager::GetEngineSourceAssetFullPath(std::wstring assetName)
 {
-    std::string prefix = "../../../Engine/Runtime/EngineSourceAssets/";
+    std::string prefix = "../../../Engine/EngineSourceAssets/";
     return std::wstring(prefix.begin(), prefix.end()) + assetName;
 }
 
 std::string IOManager::GetEditorSourceAssetFullPath(std::string assetName)
 {
-    std::string prefix = "../../../Engine/Editor/Assets/";
+    std::string prefix = "../../../Engine/EditorSourceAssets/";
     return prefix + assetName;
 }
--- Engine/Runtime/IO/IOManager.h
+++ Engine/Runtime/IO/IOManager.h
@@ -11,9 +11,9 @@ DELTA_ENGINE_NS_BEGIN
 class IOManager
 {
 public:
-	static bool readFileToBuffer(const std::string &filePath, std::vector<char>& buffer);
-	static std::wstring GetAssetFullPath(LPCWSTR assetName);
-	static std::string GetAssetFullPath(const std::string & assetName);
+	//static bool readFileToBuffer(const std::string &filePath, std::vector<char>& buffer);
+	//static std::wstring GetAssetFullPath(LPCWSTR assetName);
+	//static std::string GetAssetFullPath(const std::string & assetName);
     static std::wstring GetEngineSourceAssetFullPath(std::wstring assetName);
     DELTAENGINE_API static std::string GetEditorSourceAssetFullPath(std::string assetName);
 };
