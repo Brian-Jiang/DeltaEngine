fd96497d6e0e112b2b0cc6f1e605b85b852f4080
Xiao Jiang <bjjx1999@live.com>
2026-04-07 12:45:08 -0700

    Asio

--- Engine/ThirdParty/asio.cmake
+++ Engine/ThirdParty/asio.cmake
@@ -1,1 +1,10 @@
 message(STATUS "====== Delta Engine ====== Configuring asio ======")
+
+add_library(asio INTERFACE)
+target_include_directories(asio SYSTEM INTERFACE
+    $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/asio/include>)
+target_compile_definitions(asio INTERFACE ASIO_STANDALONE _WIN32_WINNT=0x0A00)
+
+add_library(ThirdParty::asio ALIAS asio)
+
+set_target_properties(asio PROPERTIES FOLDER ${third_party_folder})
