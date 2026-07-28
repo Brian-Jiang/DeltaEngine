7913ebc1ecb7868689b82218840e32bf1cf1d6f3
Xiao Jiang <bjjx1999@live.com>
2026-07-28 11:45:20 -0700

    Upgrade imguizmo

--- Engine/ThirdParty/imguizmo.cmake
+++ Engine/ThirdParty/imguizmo.cmake
@@ -1,20 +1,10 @@
 message(STATUS "====== Delta Engine ====== Configuring ImGuizmo ======")
 
-set(imguizmo_SOURCE_DIR_ ${CMAKE_CURRENT_SOURCE_DIR}/ImGuizmo)
+set(IMGUIZMO_BUILD_EXAMPLE OFF)
 
-file(GLOB imguizmo_sources CONFIGURE_DEPENDS
-    "${imguizmo_SOURCE_DIR_}/ImGuizmo.cpp"
-    "${imguizmo_SOURCE_DIR_}/ImSequencer.cpp"
-    "${imguizmo_SOURCE_DIR_}/ImCurveEdit.cpp"
-    "${imguizmo_SOURCE_DIR_}/ImGradient.cpp"
-    "${imguizmo_SOURCE_DIR_}/GraphEditor.cpp"
-)
+add_subdirectory(ImGuizmo)
 
-add_library(imguizmo STATIC ${imguizmo_sources})
-target_include_directories(imguizmo SYSTEM PUBLIC
-    $<BUILD_INTERFACE:${imguizmo_SOURCE_DIR_}>)
-target_link_libraries(imguizmo PUBLIC ThirdParty::imgui)
+target_link_libraries(imguizmo PUBLIC imgui)
+set_target_properties(imguizmo PROPERTIES FOLDER ${third_party_folder})
 
 add_library(ThirdParty::imguizmo ALIAS imguizmo)
-
-set_target_properties(imguizmo PROPERTIES FOLDER ${third_party_folder})
