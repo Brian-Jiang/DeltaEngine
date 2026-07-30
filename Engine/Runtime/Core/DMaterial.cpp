772c653368da782503c6ef6dedac56416ddf1b15
Xiao Jiang <bjjx1999@live.com>
2026-07-30 15:09:49 -0700

    Fix transparent object render

--- Engine/Runtime/Core/DMaterial.cpp
+++ Engine/Runtime/Core/DMaterial.cpp
@@ -17,7 +17,7 @@ CD3DX12_BLEND_DESC MakeTransparentBlendDesc()
     blend.RenderTarget[0].DestBlend = D3D12_BLEND_INV_SRC_ALPHA;
     blend.RenderTarget[0].BlendOp = D3D12_BLEND_OP_ADD;
     blend.RenderTarget[0].SrcBlendAlpha = D3D12_BLEND_ONE;
-    blend.RenderTarget[0].DestBlendAlpha = D3D12_BLEND_ZERO;
+    blend.RenderTarget[0].DestBlendAlpha = D3D12_BLEND_INV_SRC_ALPHA;
     blend.RenderTarget[0].BlendOpAlpha = D3D12_BLEND_OP_ADD;
     blend.RenderTarget[0].RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
     return blend;
